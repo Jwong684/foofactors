fb1be92c9eb40af9815815ad615354ef4229f37f
Jasper Wong <jasperwong_123@hotmail.com>
2016-12-05 12:47:12 -0800

    Added edits

--- R/fbind.R
+++ R/fbind.R
@@ -1,4 +1,4 @@
-##library(devtools)
+library(devtools)
 ##use_git()
 
 (a <- factor(c("character", "hits", "your", "eyeballs")))
@@ -7,6 +7,15 @@ c(a,b)
 ##c function does not have an intelligent method for factors,
 ##instead of making a union between two factors, you get an integer vector
 
+#' Bind two factors
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
 fbind <- function(a,b) {
 	factor(c(as.character(a), as.character(b)))
 }
